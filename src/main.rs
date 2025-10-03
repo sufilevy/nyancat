d9b3f07d37e2da0460d53b6b93c3066e74bd1e31
Sufi Levy <sufilevy@gmail.com>
2025-10-03 22:30:39 +0300

    Add example logcat sanity

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![allow(dead_code)]
 
-use crate::prelude::*;
+use anyhow::anyhow;
+
+use crate::{format::LogcatFormatter, parse::LogcatParser, prelude::*};
 
 mod filter;
 mod format;
@@ -10,5 +12,31 @@ mod parse;
 mod prelude;
 
 fn main() -> Result<()> {
+    let parser = LogcatParser::new();
+    let formatter = LogcatFormatter::new();
+
+    let full_input = include_str!("../input/logcat.txt");
+    let sanity_input = include_str!("../input/sanity.txt");
+    let stacktrace_input = include_str!("../input/stacktrace.txt");
+
+    process_input("Full", full_input, &parser, &formatter)?;
+    process_input("Stacktrace", stacktrace_input, &parser, &formatter)?;
+    process_input("Sanity", sanity_input, &parser, &formatter)?;
+
+    Ok(())
+}
+
+fn process_input(title: &str, input: &str, parser: &LogcatParser, formatter: &LogcatFormatter) -> Result<()> {
+    println!("\n*** {title} ***\n");
+
+    for line in input.lines() {
+        let log_line = parser
+            .parse_log_line(line)
+            .map_err(|e| anyhow!("failed to parse log line: '{line}' with error: '{e}'"))?;
+
+        let formatted_log_line = formatter.format_log_line(&log_line);
+        println!("{formatted_log_line}");
+    }
+
     Ok(())
 }
